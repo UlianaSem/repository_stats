76fdf0d26f13e207375d784341c459cda9e39a5a
Uliana Semyonova <i@ulianasemyonova.ru>
2023-07-30 14:44:30 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,27 +1,27 @@
-import os
-
 from functions import get_repos_stats
 from postgres_db import PostgresDB
-
-
-params = {
-    'user': os.getenv('POSTGRES_USER'),
-    'password': os.getenv('POSTGRES_PASSWORD'),
-    'host': os.getenv('POSTGRES_HOST'),
-    'port': os.getenv('POSTGRES_PORT'),
-    'dbname': os.getenv('POSTGRES_DB')
-}
+from config import config
 
 
 def main():
     """
     Задает основную логику программы
     """
-    user_name = input("Введите имя пользователя\n")
+    # получаем параметры для подключения к БД из файла
+    params = config()
+
+    # получаем имя пользователя и статистику его репозиториев
+    user_name = input("Введите имя пользователя\n").strip()
     stat = get_repos_stats(user_name)
 
+    # подключаемся к БД
     db = PostgresDB(**params)
-    db.add_data(stat)
+
+    # добавляем пользователя и статистику его репозиториев
+    user_id = db.add_user(user_name)
+    db.add_data(stat, user_id)
+
+    # экспортируем данные в JSON файл
     db.export_data_to_json()
 
 
